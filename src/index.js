3ab94c3f4158302334f113122c1d02ec97f1f3d7
Svetlana Fedorova <impari.online@yandex.ru>
2021-08-19 17:54:10 +0300

    refactor: change comments and better code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 module.exports = function reverse (n) {
     let reversedNum = 0;
-    rem = n;
+    let rem = n;
     function num_recursed(rem) {
         if (rem < 0) {
-            rem = -rem;
-           return num_recursed(rem);
+            rem = - rem;
+            return num_recursed(rem);
         }
         if (rem >= 1) {
             reversedNum = reversedNum * 10 + rem % 10;
-           return num_recursed((rem - rem % 10) / 10);
+            return num_recursed((rem - rem % 10) / 10);
          }
-         else return reversedNum;
-        
+        else return reversedNum;        
         } 
 
  //delete this to return negative as negative
     return num_recursed(n);
- // a crunsh to return reversed negative numbers as negative numbers;
- //   if (n >= 0) {return num_recursed(n);}
- //   else { let num = num_recursed(n);
- //          num = - num;
- //          return num; }
+
+ // a crutch to return reversed negative numbers as negative numbers
+ //   if (n >= 0) {return num_recursed(rem);}
+ //   else { let num = num_recursed(rem);       
+ //          return -num; }
 }

397f21e18f064a31eb2266c250ac65c27b50f1c7
Svetlana Fedorova <impari.online@yandex.ru>
2021-08-19 14:35:25 +0300

    feat: function complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+    let reversedNum = 0;
+    rem = n;
+    function num_recursed(rem) {
+        if (rem < 0) {
+            rem = -rem;
+           return num_recursed(rem);
+        }
+        if (rem >= 1) {
+            reversedNum = reversedNum * 10 + rem % 10;
+           return num_recursed((rem - rem % 10) / 10);
+         }
+         else return reversedNum;
+        
+        } 
+
+ //delete this to return negative as negative
+    return num_recursed(n);
+ // a crunsh to return reversed negative numbers as negative numbers;
+ //   if (n >= 0) {return num_recursed(n);}
+ //   else { let num = num_recursed(n);
+ //          num = - num;
+ //          return num; }
 }
